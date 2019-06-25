eaf0e40b2fc1452872a0c9ea69050a980a18503b
boris <bashmachok.ua@gmail.com>
2019-06-25 14:18:05 +0200

    lakalute

--- app/src/main/java/com/example/boris/wallpaperslideshow/DroidWallpaper.java
+++ app/src/main/java/com/example/boris/wallpaperslideshow/DroidWallpaper.java
@@ -15,11 +15,11 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.widget.Toast;
 
+import com.example.boris.wallpaperslideshow.figures.InfinitySnake;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class DroidWallpaper extends WallpaperService {
@@ -28,16 +28,11 @@ public class DroidWallpaper extends WallpaperService {
     private long lastPressTime = 0, lastTimeAll = 0;
     private Matrix matrix = new Matrix();
     private long duration = 0;
-    private boolean endThread = false, normalMode = true, flyToFinger = false, firstReady = false;
-    private int width = Resources.getSystem().getDisplayMetrics().widthPixels, height = Resources.getSystem().getDisplayMetrics().heightPixels;
-    private float fireX, fireY, X = 0.01f, fingerX, fingerY, distanceX, distanceY, lastX, lastY;
-    private List<Float> arrX = new ArrayList<>(), arrY = new ArrayList<>();
-
-    private float speedOfCircles = 0.02f;
-    private int numberOfCircles = 60, sizeOfCircles = 30;
+    private InfinitySnake snake;
 
     @Override
     public WallpaperService.Engine onCreateEngine() {
+        snake = new InfinitySnake(getApplicationContext());
         return new DroidWallpaperEngine();
     }
 
@@ -68,9 +63,8 @@ public class DroidWallpaper extends WallpaperService {
 
         private long frameDuration = TimeUnit.SECONDS.toMillis(5);
         private SurfaceHolder holder;
-        private boolean visible;
+        private boolean visible, endThread = false;
         private Handler handler;
-        private boolean flyBack;
 
         public DroidWallpaperEngine() {
             handler = new Handler();
@@ -101,25 +95,21 @@ public class DroidWallpaper extends WallpaperService {
                 //makeErrorNotification("drawing: " + currentImage + " of " + numberOfImages + " \nname: " + files[currentImage - 1].getName() + "\nbitmap: " + bitmap.toString());
                 Bitmap finalBitmap = bitmap;
                 Paint paint = new Paint();
-                paint.setStrokeWidth(8);
-                paint.setStyle(Paint.Style.FILL);
+                snake.setPaint(paint);
                 Path path1 = new Path();
                 new Thread(() -> {
                     while (lastTimeAll + frameDuration - 100 > System.currentTimeMillis() && !endThread) {
                         if (!holder.isCreating()) {
                             Canvas canvas = holder.lockCanvas();
-                            //canvas.save();
                             canvas.drawBitmap(finalBitmap, matrix, null); // draw main picture
-                            //canvas.restore();
 
                             //draw fire
-                            drowFire(canvas, path1, paint);
+                            snake.drowFire(canvas, path1, paint);
                             holder.unlockCanvasAndPost(canvas);
                         }
                     }
                     endThread = false;
                 }).start();
-
 
                 lastTimeAll = System.currentTimeMillis();
                 //get next slide duration
@@ -134,99 +124,6 @@ public class DroidWallpaper extends WallpaperService {
             }
         }
 
-        private void drowFire(Canvas canvas, Path path1, Paint paint) {
-            if (normalMode){
-                arrX.add(getX(X));
-                arrY.add(getY(X));
-                if(arrX.size() > numberOfCircles){
-                    float i = 0;
-                    for (int n = arrX.size() - numberOfCircles - 1; n < arrX.size() ; n++) {
-                        path1.addCircle(arrX.get(n), arrY.get(n), 5 + (i++ / 100 * sizeOfCircles), Path.Direction.CW);
-                    }
-                }else{
-                    int i = 0;
-                    for (float n = 0; n < numberOfCircles / 100; n += 0.01) {
-                        path1.addCircle(getX(X + n), getY(X + n), 5 + (n * sizeOfCircles), Path.Direction.CW);
-                        arrX.add(i, getX(X + n)); //record last value X
-                        arrY.add(i++, getY(X + n)); //record last value Y
-                    }
-                }
-                X += speedOfCircles;
-            }
-            if (flyToFinger) {
-                arrX.add(getXfly());
-                arrY.add(getYfly());
-                float i = 0;
-                for (int n = arrX.size() - numberOfCircles - 1; n < arrX.size(); n++) {
-                    path1.addCircle(arrX.get(n), arrY.get(n), 5 + (i++ / 100 * sizeOfCircles), Path.Direction.CW);
-                }
-            }
-            if (flyBack) {
-                arrX.add(getXfly());
-                arrY.add(getYfly());
-                float i = 0;
-                for (int n = arrX.size() - numberOfCircles - 1; n < arrX.size(); n++) {
-                    path1.addCircle(arrX.get(n), arrY.get(n), 5 + (i++ / 100 * sizeOfCircles), Path.Direction.CW);
-                }
-            }
-
-            canvas.drawPath(path1, paint);
-            path1.reset();
-        }
-
-        private float getX(float i) {
-            //float scale = 2 / (3 - (float)Math.cos(2 * i));
-            fireX = (float) Math.cos(i) * 270 + width / 2;
-            lastX = fireX;
-            return fireX;
-        }
-
-        private float getY(float i) {
-            //double scale = 2 / (3 - Math.cos(2 * i));
-            fireY = (float) ((Math.sin(2 * i) / 2) * 270 + height / 2);
-            lastY = fireY;
-            return fireY;
-        }
-
-        private float getXfly() {
-            if (flyToFinger) {
-                if ((fingerX - fireX > 0 && fingerX - fireX > 10) || (fingerX - fireX < 0 && fingerX - fireX < 10))
-                    fireX += distanceX / 50;
-            }
-            if (flyBack) {
-                if ((lastX - fireX > 0 && lastX - fireX > 5) || (lastX - fireX < 0 && lastX - fireX < 5))
-                    fireX += distanceX / 50;
-                else if (firstReady) {
-                    flyBack = false;
-                    normalMode = true;
-                    firstReady = false;
-                }else{
-                    firstReady = true;
-                }
-            }
-            return fireX;
-        }
-
-        private float getYfly() {
-            if (flyToFinger) {
-                if ((fingerY - fireY > 0 && fingerY - fireY > 10) || (fingerY - fireY < 0 && fingerY - fireY < 10))
-                    fireY += distanceY / 50;
-            }
-            if (flyBack) {
-                if ((lastY - fireY > 0 && lastY - fireY > 5) || (lastY - fireY < 0 && lastY - fireY < 5))
-                    fireY += distanceY / 50;
-                else if (firstReady) {
-                    flyBack = false;
-                    normalMode = true;
-                    firstReady = false;
-                }else{
-                    firstReady = true;
-                }
-            }
-
-            return fireY;
-        }
-
         @Override
         public void onVisibilityChanged(boolean visible) {
             this.visible = visible;
@@ -234,11 +131,8 @@ public class DroidWallpaper extends WallpaperService {
                 if (lastTimeAll + TimeUnit.SECONDS.toMillis(duration) < System.currentTimeMillis())
                     handler.post(drawImage);
             } else {
-                normalMode = true;
-                flyBack = false;
-                flyToFinger = false;
-                arrX.clear();
-                arrY.clear();
+                snake.setMode(true, false, false);
+                snake.clearArrays();
                 handler.removeCallbacks(drawImage);
             }
         }
@@ -246,8 +140,7 @@ public class DroidWallpaper extends WallpaperService {
         @Override
         public void onDestroy() {
             super.onDestroy();
-            arrX.clear();
-            arrY.clear();
+            snake.clearArrays();
             handler.removeCallbacks(drawImage);
         }
 
@@ -263,17 +156,10 @@ public class DroidWallpaper extends WallpaperService {
         public void onTouchEvent(MotionEvent event) {
             super.onTouchEvent(event);
 
-            fingerX = event.getX();
-            fingerY = event.getY();
-            if (flyToFinger) {
-                distanceX = fingerX - fireX;
-                distanceY = fingerY - fireY;
-            }
+            snake.setFingers(event.getX(), event.getY());
 
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                flyToFinger = true;
-                normalMode = false;
-                flyBack = false;
+                snake.setMode(false,true,false);
                 //double click event
                 if (System.currentTimeMillis() - lastPressTime < 500) {
                     lastPressTime = 0;
@@ -284,13 +170,10 @@ public class DroidWallpaper extends WallpaperService {
                 }
             }
             if (event.getAction() == MotionEvent.ACTION_UP) {
-                flyToFinger = false;
-                flyBack = true;
-                normalMode = false;
-
-                distanceX = lastX - fireX;
-                distanceY = lastY - fireY;
+                snake.setMode(false,false,true);
             }
+            snake.setDistances();
+
         }
     }
 
---
+++ app/src/main/java/com/example/boris/wallpaperslideshow/figures/InfinitySnake.java
@@ -0,0 +1,163 @@
+package com.example.boris.wallpaperslideshow.figures;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+
+import com.example.boris.wallpaperslideshow.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.core.content.ContextCompat;
+
+public class InfinitySnake {
+    private Context context;
+    private float fireX, fireY, X = 0.01f, fingerX, fingerY, distanceX, distanceY, lastX, lastY;
+    private List<Float> arrX = new ArrayList<>(), arrY = new ArrayList<>();
+    private int width = Resources.getSystem().getDisplayMetrics().widthPixels, height = Resources.getSystem().getDisplayMetrics().heightPixels;
+
+    private float speedOfCircles = 0.02f;
+    private int numberOfCircles = 60, sizeOfCircles = 30;
+    private boolean normalMode = true, flyToFinger = false, flyBack = false, firstReady = false;
+
+    public InfinitySnake(Context context) {
+        this.context = context;
+    }
+
+    public void drowFire(Canvas canvas, Path path1, Paint paint) {
+        if (normalMode){
+            arrX.add(getX(X));
+            arrY.add(getY(X));
+            paint.setShader(new RadialGradient(fireX, fireY,
+                    sizeOfCircles,  ContextCompat.getColor(context,R.color.red), ContextCompat.getColor(context,R.color.orange), Shader.TileMode.MIRROR));
+
+            if(arrX.size() > numberOfCircles){
+                float i = 0;
+                for (int n = arrX.size() - numberOfCircles - 1; n < arrX.size() ; n++) {
+                    path1.addCircle(arrX.get(n), arrY.get(n), 5 + (i++ / 100 * sizeOfCircles), Path.Direction.CW);
+                }
+            }else{
+                int i = 0;
+                for (float n = 0; n < numberOfCircles / 100; n += 0.01) {
+                    path1.addCircle(getX(X + n), getY(X + n), 5 + (n * sizeOfCircles), Path.Direction.CW);
+                    arrX.add(i, getX(X + n)); //record last value X
+                    arrY.add(i++, getY(X + n)); //record last value Y
+                }
+            }
+            X += speedOfCircles;
+        }
+        if (flyToFinger) {
+            arrX.add(getXfly());
+            arrY.add(getYfly());
+            float i = 0;
+            for (int n = arrX.size() - numberOfCircles - 1; n < arrX.size(); n++) {
+                path1.addCircle(arrX.get(n), arrY.get(n), 5 + (i++ / 100 * sizeOfCircles), Path.Direction.CW);
+            }
+        }
+        if (flyBack) {
+            arrX.add(getXfly());
+            arrY.add(getYfly());
+            float i = 0;
+            for (int n = arrX.size() - numberOfCircles - 1; n < arrX.size(); n++) {
+                path1.addCircle(arrX.get(n), arrY.get(n), 5 + (i++ / 100 * sizeOfCircles), Path.Direction.CW);
+            }
+        }
+
+        canvas.drawPath(path1, paint);
+        path1.reset();
+    }
+
+    public void setPaint(Paint paint){
+        Float positions = null; //floatArrayOf(0f, 0.3f, 0.6f)
+
+        // Gradient Shade colors
+        paint.setStrokeWidth(8);
+
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    private float getX(float i) {
+        //float scale = 2 / (3 - (float)Math.cos(2 * i));
+        fireX = (float) Math.cos(i) * 270 + width / 2;
+        lastX = fireX;
+        return fireX;
+    }
+
+    private float getY(float i) {
+        //double scale = 2 / (3 - Math.cos(2 * i));
+        fireY = (float) ((Math.sin(2 * i) / 2) * 270 + height / 2);
+        lastY = fireY;
+        return fireY;
+    }
+
+    private float getXfly() {
+        if (flyToFinger) {
+            if ((fingerX - fireX > 0 && fingerX - fireX > 10) || (fingerX - fireX < 0 && fingerX - fireX < 10))
+                fireX += distanceX / 60;
+        }
+        if (flyBack) {
+            if ((lastX - fireX > 0 && lastX - fireX > 5) || (lastX - fireX < 0 && lastX - fireX < 5))
+                fireX += distanceX / 60;
+            else if (firstReady) {
+                flyBack = false;
+                normalMode = true;
+                firstReady = false;
+            }else{
+                firstReady = true;
+            }
+        }
+        return fireX;
+    }
+
+    private float getYfly() {
+        if (flyToFinger) {
+            if ((fingerY - fireY > 0 && fingerY - fireY > 10) || (fingerY - fireY < 0 && fingerY - fireY < 10))
+                fireY += distanceY / 60;
+        }
+        if (flyBack) {
+            if ((lastY - fireY > 0 && lastY - fireY > 5) || (lastY - fireY < 0 && lastY - fireY < 5))
+                fireY += distanceY / 60;
+            else if (firstReady) {
+                flyBack = false;
+                normalMode = true;
+                firstReady = false;
+            }else{
+                firstReady = true;
+            }
+        }
+
+        return fireY;
+    }
+
+    public void setMode(boolean normalMode, boolean flyToFinger, boolean flyBack) {
+        this.normalMode = normalMode;
+        this.flyToFinger = flyToFinger;
+        this.flyBack = flyBack;
+    }
+
+    public void clearArrays(){
+        arrX.clear();
+        arrY.clear();
+    }
+
+    public void setFingers(float fingerX, float fingerY){
+        this.fingerX = fingerX;
+        this.fingerY = fingerY;
+    }
+
+    public void setDistances() {
+        if (flyToFinger) {
+            distanceX = fingerX - fireX;
+            distanceY = fingerY - fireY;
+        }
+        if (flyBack){
+            distanceX = lastX - fireX;
+            distanceY = lastY - fireY;
+        }
+    }
+}
